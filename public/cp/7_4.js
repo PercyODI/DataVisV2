27929ec4770ccae36a910d6fe709f13274ce64ef
Pearse Hutson <pearse.2008@gmail.com>
2017-10-27 16:33:46 -0500

    Eslint things

--- public/cp/7_4.js
+++ public/cp/7_4.js
@@ -30,36 +30,36 @@ var yScale = d3
         0, d3.max(dataset, d => d[1])
     ])
     .range([h - padding, padding]);
-    
+
 var aScale = d3.scaleSqrt()
     .domain([0, d3.max(dataset, d => d[1])])
     .range([0, 10]);
 
 var svg = d3
-    .select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .style("border", "1px solid black");
+    .select('body')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .style('border', '1px solid black');
 
 svg
-    .selectAll("circle")
+    .selectAll('circle')
     .data(dataset)
     .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d[0]))
-    .attr("cy", d => yScale(d[1]))
-    .attr("r", d => aScale(d[1]))
-    .attr("fill", d => `rgb(0, 0, ${Math.round(d * 10)})`);
+    .append('circle')
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', d => aScale(d[1]))
+    .attr('fill', d => `rgb(0, 0, ${Math.round(d * 10)})`);
 
 svg
-    .selectAll("text")
+    .selectAll('text')
     .data(dataset)
     .enter()
-    .append("text")
-    .attr("x", d => xScale(d[0]))
-    .attr("y", d => yScale(d[1]))
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "11px")
-    .attr("fill", "red")
+    .append('text')
+    .attr('x', d => xScale(d[0]))
+    .attr('y', d => yScale(d[1]))
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', '11px')
+    .attr('fill', 'red')
     .text(d => `${d[0]},${d[1]}`);
