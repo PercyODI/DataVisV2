2f4bf0014487ee04fb5c448a96a77e228260c5c2
Pearse Hutson <pearse.2008@gmail.com>
2017-10-26 20:49:42 -0500

    Up to 7_4

--- public/cp/7_1.js
+++ public/cp/7_1.js
@@ -1,3 +1,9 @@
 var dataset = [100, 200, 300, 400, 500];
 
-var scale = d3
+var scale = d3.scaleLinear()
+    .domain([100, 500])
+    .range([10, 350]);
+
+console.log(scale(100));
+console.log(scale(300));
+console.log(scale(500));
---
+++ public/cp/7_3.js
@@ -0,0 +1,60 @@
+var w = 500;
+var h = 100;
+var barPadding = 1;
+var padding = 20;
+
+var dataset = [
+    [5, 20],
+    [480, 90],
+    [250, 50],
+    [100, 33],
+    [330, 95],
+    [410, 12],
+    [475, 44],
+    [25, 67],
+    [85, 21],
+    [220, 88]
+];
+
+var xScale = d3
+    .scaleLinear()
+    .domain([
+        0, d3.max(dataset, d => d[0])
+    ])
+    .range([padding * 2, w - (padding * 2)]);
+
+var yScale = d3
+    .scaleLinear()
+    .domain([
+        0, d3.max(dataset, d => d[1])
+    ])
+    .range([h - padding, padding]);
+
+var svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .style("border", "1px solid black");
+
+svg
+    .selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d[0]))
+    .attr("cy", d => yScale(d[1]))
+    .attr("r", d => Math.sqrt(h - d[1]))
+    .attr("fill", d => `rgb(0, 0, ${Math.round(d * 10)})`);
+
+svg
+    .selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .attr("x", d => xScale(d[0]))
+    .attr("y", d => yScale(d[1]))
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "red")
+    .text(d => `${d[0]},${d[1]}`);
